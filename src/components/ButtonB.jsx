0abb20b17dd547d28aca9d08fa96912366a437b3
Salvador <salvador.on@outlook.com>
2020-06-01 16:49:23 -0300

    fix eslinter issue 1

--- src/components/ButtonB.jsx
+++ src/components/ButtonB.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 const ButtonB = ({ name, wide, color}) => (
   <React.Fragment>
-    <button type="button" value={name} className={ (color || 'orange')} style={{ width: (wide ? '50%' : '25%'), height: '100px' }}>
+    <button type="button" value={name} className={(color || 'orange')} style={{ width: (wide ? '50%' : '25%'), height: '100px' }}>
       {name}
     </button>
   </React.Fragment>
@@ -12,7 +12,6 @@ const ButtonB = ({ name, wide, color}) => (
 ButtonB.propTypes = {
   name: PropTypes.string.isRequired,
   wide: PropTypes.bool.isRequired,
-  color: PropTypes.string,
 };
 
 export default ButtonB;
