e5e899f729d958b55a1230c8e5da675a4cca5b5f
Salvador <salvador.on@outlook.com>
2020-06-01 15:52:49 -0300

    fix slinter issues 4

--- src/components/ButtonB.jsx
+++ src/components/ButtonB.jsx
@@ -1,14 +1,13 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
 const ButtonB = ({ name, wide }) => (
-    <React.Fragment>
-      <button type="button" value={name} style={{ width: wide, height: '75px' }}>
-        {name}
-      </button>
-    </React.Fragment>
-  );
-
+  <React.Fragment>
+    <button type="button" value={name} style={{ width: wide, height: "75px" }}>
+      {name}
+    </button>
+  </React.Fragment>
+);
 
 ButtonB.propTypes = {
   name: PropTypes.string.isRequired,
