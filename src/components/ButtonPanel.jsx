36ddc0753969b5985d01368c634f63e2b304f8a2
Salvador <salvador.on@outlook.com>
2020-06-01 15:55:54 -0300

    fix slinter issues 6

--- src/components/ButtonPanel.jsx
+++ src/components/ButtonPanel.jsx
@@ -1,40 +1,39 @@
 import React from 'react';
 import ButtonB from './ButtonB';
 
-const ButtonPanel = () => {
-  return (
+const ButtonPanel = () => (
     <React.Fragment>
       <div className="Group">
-        <ButtonB name='AC' wide={"25%"}/>
-        <ButtonB name={"+/-"} wide={"25%"}/>
-        <ButtonB name={"%"} wide={"25%"}/>
-        <ButtonB name={"÷"} wide={"25%"}/>
+        <ButtonB name='AC' wide='25%'/>
+        <ButtonB name='+/-' wide='25%'/>
+        <ButtonB name='%' wide='25%'/>
+        <ButtonB name='÷' wide='25%'/>
       </div>
       <div className="Group">
-        <ButtonB name={"7"} wide={"25%"}/>
-        <ButtonB name={"8"} wide={"25%"}/>
-        <ButtonB name={"9"} wide={"25%"}/>
-        <ButtonB name={"X"} wide={"25%"}/>
+        <ButtonB name='7' wide='25%'/>
+        <ButtonB name='8' wide='25%'/>
+        <ButtonB name='9' wide='25%'/>
+        <ButtonB name='X' wide='25%'/>
       </div>
       <div className="Group">
-        <ButtonB name={"4"} wide={"25%"}/>
-        <ButtonB name={"5"} wide={"25%"}/>
-        <ButtonB name={"6"} wide={"25%"}/>
-        <ButtonB name={"-"} wide={"25%"}/>
+        <ButtonB name='4' wide='25%'/>
+        <ButtonB name='5' wide='25%'/>
+        <ButtonB name='6' wide='25%'/>
+        <ButtonB name='-' wide='25%'/>
       </div>
       <div className="Group1">
-        <ButtonB name={"1"} wide={"25%"}/>
-        <ButtonB name={"2"} wide={"25%"}/>
-        <ButtonB name={"3"} wide={"25%"}/>
-        <ButtonB name={"+"} wide={"25%"}/>
+        <ButtonB name='1' wide='25%'/>
+        <ButtonB name='2' wide='25%'/>
+        <ButtonB name='3' wide='25%'/>
+        <ButtonB name='+' wide='25%'/>
       </div>
       <div className="Group1">
-        <ButtonB name={"0"} wide={"50%"}/>
-        <ButtonB name={"."} wide={"25%"}/>
-        <ButtonB name={"="} wide={"25%"}/>
+        <ButtonB name='0' wide='50%'/>
+        <ButtonB name='.' wide='25%'/>
+        <ButtonB name='=' wide='25%'/>
       </div>
     </React.Fragment>
   );
-};
+
 
 export default ButtonPanel;
