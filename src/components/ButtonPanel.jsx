8a8cd59e6496a8c57599a5f9f82db24af353ed91
Salvador <salvador.on@outlook.com>
2020-06-01 16:55:04 -0300

    fix eslinter issue 5

--- src/components/ButtonPanel.jsx
+++ src/components/ButtonPanel.jsx
@@ -7,25 +7,25 @@ const ButtonPanel = () => (
       <ButtonB name="AC" wide={false} color="grey" />
       <ButtonB name="+/-" wide={false} color="grey" />
       <ButtonB name="%" wide={false} color="grey" />
-      <ButtonB name="÷" wide={false}/>
+      <ButtonB name="÷" wide={false} />
     </div>
     <div className="Group">
       <ButtonB name="7" wide={false} color="grey" />
       <ButtonB name="8" wide={false} color="grey" />
       <ButtonB name="9" wide={false} color="grey" />
-      <ButtonB name="X" wide={false}/>
+      <ButtonB name="X" wide={false} />
     </div>
     <div className="Group">
       <ButtonB name="4" wide={false} color="grey" />
       <ButtonB name="5" wide={false} color="grey" />
       <ButtonB name="6" wide={false} color="grey" />
-      <ButtonB name="-" wide={false}/>
+      <ButtonB name="-" wide={false} />
     </div>
     <div className="Group1">
       <ButtonB name="1" wide={false} color="grey" />
       <ButtonB name="2" wide={false} color="grey" />
       <ButtonB name="3" wide={false} color="grey" />
-      <ButtonB name="+" wide={false}/>
+      <ButtonB name="+" wide={false} />
     </div>
     <div className="Group1">
       <ButtonB name="0" wide={true} color="grey" />
