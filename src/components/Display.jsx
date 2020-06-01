8dcaee51d3587cd07957394ddf2d3502e36246bd
Salvador <salvador.on@outlook.com>
2020-06-01 16:03:28 -0300

    fix slinter issues 10

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,16 +1,20 @@
-import React from 'react';
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
 
-const Display = ({result}) => {
-  return (<React.Fragment>
-    <div className="DisplayScreen">
-      <h1 className="Result"> {result}</h1>
-     </div>
-    </React.Fragment>  );
-}
+const Display = ({ result }) => {
+  return (
+    <React.Fragment>
+      <div className="DisplayScreen">
+        <h1 className="Result">
+          {result}
+        </h1>
+      </div>
+    </React.Fragment>
+  );
+};
 
 Display.propTypes = {
   result: PropTypes.string.isRequired,
-}
- 
+};
+
 export default Display;
