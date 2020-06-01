72f7e9e4bd0cc88bac272496fc2065ad89c43d9f
Salvador <salvador.on@outlook.com>
2020-06-01 16:46:18 -0300

    add styles color prop

--- src/components/ButtonPanel.jsx
+++ src/components/ButtonPanel.jsx
@@ -7,30 +7,30 @@ const ButtonPanel = () => (
       <ButtonB name="AC" wide={ false } color="grey"/>
       <ButtonB name="+/-" wide={ false } color="grey"/>
       <ButtonB name="%" wide={ false } color="grey"/>
-      <ButtonB name="÷" wide={ false } color="orange"/>
+      <ButtonB name="÷" wide={ false }/>
     </div>
     <div className="Group">
       <ButtonB name="7" wide={ false } color="grey"/>
       <ButtonB name="8" wide={ false } color="grey"/>
       <ButtonB name="9" wide={ false } color="grey"/>
-      <ButtonB name="X" wide={ false } color="orange"/>
+      <ButtonB name="X" wide={ false }/>
     </div>
     <div className="Group">
       <ButtonB name="4" wide={ false } color="grey"/>
       <ButtonB name="5" wide={ false } color="grey"/>
       <ButtonB name="6" wide={ false } color="grey"/>
-      <ButtonB name="-" wide={ false } color="orange"/>
+      <ButtonB name="-" wide={ false }/>
     </div>
     <div className="Group1">
       <ButtonB name="1" wide={ false } color="grey"/>
       <ButtonB name="2" wide={ false } color="grey"/>
       <ButtonB name="3" wide={ false } color="grey"/>
-      <ButtonB name="+" wide={ false } color="orange"/>
+      <ButtonB name="+" wide={ false }/>
     </div>
     <div className="Group1">
       <ButtonB name="0" wide= { true } color="grey"/>
       <ButtonB name="." wide={ false } color="grey"/>
-      <ButtonB name="=" wide={ false } color="orange"/>
+      <ButtonB name="=" wide={ false }/>
     </div>
   </React.Fragment>
 );
