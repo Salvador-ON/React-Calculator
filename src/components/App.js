9fdadc32eba2754072686fe775927ddb5f6ca637
Salvador <salvador.on@outlook.com>
2020-06-03 09:07:52 -0300

    fix issues

--- src/components/App.js
+++ src/components/App.js
@@ -1,36 +1,43 @@
-import React from 'react';
-import Display from './Display';
-import ButtonPanel from './ButtonPanel';
-import calculate from '../logic/calculate';
+import React from "react";
+import Display from "./Display";
+import ButtonPanel from "./ButtonPanel";
+import calculate from "../logic/calculate";
 
 function App() {
   const [data, SetData] = React.useState({
-    total: '',
-    currentValue: '',
-    operation: '',
+    total: "",
+    next: "",
+    operation: "",
   });
 
-  const SetCV = value => {
-    const { total, currentValue, operation } = value;
+  const SetCV = (value) => {
+    const { total, next, operation } = value;
     SetData({
       total,
-      currentValue,
+      next,
       operation,
     });
   };
 
-  const LogicOp = value => {
+  const LogicOp = (value) => {
     const dataObject = calculate(data, value);
     SetCV(dataObject);
   };
 
-  const getValue = e => {
+  const getValue = (e) => {
     LogicOp(e.target.value);
   };
 
   return (
     <div className="Frame">
-      <Display result={data.currentValue || (data.total === '' ? '0' : '') || (data.currentValue === '' && data.operation === '' ? data.total : '') || (data.currentValue === '' && data.operation !== '' ? data.operation : '')} />
+      <Display
+        result={
+          data.next ||
+          (data.total === "" ? "0" : "") ||
+          (data.next === "" && data.operation === "" ? data.total : "") ||
+          (data.next === "" && data.operation !== "" ? data.operation : "")
+        }
+      />
       <ButtonPanel getValue={getValue} />
     </div>
   );
